34aba5cec76c02ae8077e782d3132364167253fa
Dan Paddock <danpaddock@dans-air.lan>
2020-08-18 16:36:43 -0400

    portfolio setup

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+const ExpenseDashboardPage = () => (
+    <div>
+        Text from ExpenseDashboardPage
+    </div>
+);
+
+export default ExpenseDashboardPage;
