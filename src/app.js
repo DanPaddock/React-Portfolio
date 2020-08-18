061bcf573ce92be5f56732b6ef3b0919344d5014
Dan Paddock <danpaddock@dans-air.lan>
2020-08-18 13:01:28 -0400

    added basic page routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+    <div>
+        Text from ExpenseDashboardPage
+    </div>
+);
+
+const AddExpensePage = () => (
+    <div>
+        Text from AddExpensePage
+    </div>
+);
+
+const EditExpensePage = () => (
+    <div>
+        Text from EditExpensePage
+    </div>
+);
+
+const HelpPage = () => (
+    <div>
+        Text from HelpPage
+    </div>
+);
+
+const routes = (
+    <BrowserRouter>
+    <div>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/help" component={HelpPage}/>
+    </div>
+    </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
